9b6e63f60c1b0af941bb6ac928331c89f2d286ef
yotsaphonSutweha <komsan_58@hotmail.com>
2018-07-22 00:09:19 +0100

    accept the value of city from command line and return the current temperature

--- app.js
+++ app.js
@@ -1,19 +1,31 @@
 const https = require('https');
-const url = "https://samples.openweathermap.org/data/2.5/forecast?q=M%C3%BCnchen,DE&appid=78420792cdac464da286f5052d2ce008";
 
-const request = https.get(url, response => {
-    let body = "";
-    // Heppens when data stream is started
-    response.on('data', data => {
-        // Read the data
-        body += data.toString();
-    });
+const displayMessage = (currentTemp) => {
+    let tempInDegree = currentTemp - 273.15;
+    let msg = `The current temperature is ${Math.floor(tempInDegree)} degrees`;
+    console.log(msg);
+}
 
-    // End stream
-    response.on('end', () => {
-        // Parsing to JSON
-        let weather = "";
-        weather = JSON.parse(body);
-        console.log(weather.list[0].main.temp);
+const getWeather = (city) => {
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=78420792cdac464da286f5052d2ce008`;
+    const request = https.get(url, response => {
+        let body = "";
+        // Heppens when data stream is started
+        response.on('data', data => {
+            // Read the data
+            body += data.toString();
+        });
+
+        // End stream
+        response.on('end', () => {
+            // Parsing to JSON
+            let weather = "";
+            weather = JSON.parse(body);
+            // Print out message 
+            displayMessage(weather.main.temp);
+        });
     });
-});
+}
+
+let city = process.argv.slice(2);
+getWeather(city);
